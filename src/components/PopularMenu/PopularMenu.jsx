b0a9543e5a1bf5c791533bd9354c1a57f9cf9e17
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-11-29 20:44:09 +0600

    popular menu design updated

--- src/components/PopularMenu/PopularMenu.jsx
+++ src/components/PopularMenu/PopularMenu.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import MenuItems from "../MenuItems/MenuItems";
+import { Link } from "react-router-dom";
 
 const PopularMenu = () => {
   const [menu, setMenu] = useState([]);
@@ -23,6 +24,15 @@ const PopularMenu = () => {
         {menu.map((item) => (
           <MenuItems key={item._id} item={item} />
         ))}
+      </div>
+      <div className="text-center">
+        <Link>
+          <button className="btn">View Full Menu</button>
+        </Link>
+      </div>
+
+      <div className="bg-black text-white text-center text-2xl font-bold p-12 my-10 ">
+        Call us: +88 123456789
       </div>
     </section>
   );
